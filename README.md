7cbc6dce1312f78f01c7958245f8e6ff9f7dcd3a
Chonnarong Hanyawongse <chonnarong@nonagon.co.th>
2018-08-11 12:09:33 +0700

    Initial conversion script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # parse-to-firebase
-JSON user data conversion tool to convert from Parse to Firebase
+JSON Users data conversion tool to convert from Parse to the structure that is able to import to Firebase.
+
+Setup:
+```
+yarn install
+```
+
+Usage:
+```
+node ./convert-users --input=path/to/parse-users.json --output=path/to/output.json [--includeSocials]
+```
---
+++ convert-users.js
@@ -0,0 +1,59 @@
+const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
+
+if (!argv.in && !argv.out) {
+    console.info('Usage: node convert-users --in=path/to/source --out=path/to/output [--includeSocials]');
+    return;
+}
+
+const inputData = require(argv.in);
+const outputData = {
+    users: []
+};
+
+inputData.forEach(user => {
+    if (user.email && !user.authDataThirdParty) {
+        let password = user._hashed_password;
+        if (!password) {
+            password = "";
+        }
+        const passwordUser = {
+            localId: user._id,
+            email: user.email,
+            displayName: user.username,
+            passwordHash: Buffer.from(password).toString('base64'),
+        }
+        outputData.users.push(passwordUser);
+    }
+
+    if (!argv.includeSocials) {
+        return;
+    }
+
+    if (user._auth_data_facebook) {
+        const fbUser = {
+            localId: user._id,
+            displayName: user.username,
+            providerUserInfo: [{
+                providerId: 'facebook.com',
+                rawId: user._auth_data_facebook.id
+            }]
+        }
+        outputData.users.push(fbUser);
+    }
+
+    if (user.authDataThirdParty && JSON.parse(user.authDataThirdParty).google) {
+        const gUser = {
+            localId: user._id,
+            email: user.email,
+            displayName: user.username,
+            providerUserInfo: [{
+                providerId: 'google.com',
+                rawId: JSON.parse(user.authDataThirdParty).google.id,
+            }]
+        }
+        outputData.users.push(gUser);
+    }
+});
+
+fs.writeFileSync(argv.out, JSON.stringify(outputData));
